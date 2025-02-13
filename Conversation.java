062f318fffdc24517195c96c957de5f551970f30
Anna <afeuer@smith.edu.com>
2025-02-13 12:09:34 -0500

    sorta works

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner; 
+import java.util.Random;
 
 class Conversation implements Chatbot {
 
-  // Attributes 
+  String input; //string to store user input
+  ArrayList<String> transcript = new ArrayList<String>(); //array list to store conversation transcript 
+  ArrayList<String> randRespond = new ArrayList<String>(); //array list to store canned responses
+  
+  //Constructor 
+  public Conversation() {
+    //add canned responses to array list
+    randRespond.add("I see.");
+    randRespond.add("Hmm...");
+    randRespond.add("That's so cool!");
+    randRespond.add("Really?");
+  }
 
-  /**
-   * Constructor 
-   */
-  Conversation() {
+  //Starts and runs the conversation with the user
+  public void chat() {
+    Scanner in = new Scanner(System.in); // Create a Scanner object
+    
+    System.out.println("Hello! How many rounds of conversation would you like to have?");
+    int rounds = in.nextInt(); //define rounds as user input
+    in.nextLine(); //throw out the newline character
+    
+    System.out.println("Great! What would you like to talk about?");
+
+    //for loop that runs the number of rounds of conversation
+    for (int i = 0; i < rounds; i++) {
+      input = in.nextLine(); //take in user input as string
+      in.nextLine();
+      transcript.add(input); //add user input to transcript
+      System.out.println(randRespond.get(i)); //respond with canned response
+      transcript.add(randRespond.get(i)); //add canned response to transcript
+    }
+
+    System.out.println("Thanks for chatting!\nHere's a transcript of our conversation:");
     
   }
 
-  /**
-   * Starts and runs the conversation with the user
-   */
-  public void chat() {
-
-  }
-
-  /**
-   * Prints transcript of conversation
-   */
+  //Prints transcript of conversation
   public void printTranscript() {
-
+    for (int i = 0; i < transcript.size(); i++) {
+      System.out.println(transcript.get(i));
+    }
   }
 
   /**
