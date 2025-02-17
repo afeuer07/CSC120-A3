4227a81e586a96bdba212ce8fb22257c4a3f3f57
Anna <afeuer@smith.edu.com>
2025-02-17 15:34:05 -0500

    basically all works

--- Conversation.java
+++ Conversation.java
@@ -1,17 +1,16 @@
-//as of 2/13 2pm:
-//the transcript printing works 100%
-//the loop runs the correct numeber of times
-//the canned responses are random and printed correctly
-//need to:
-//add a respond method to find mirror words and decide when to use them vs random
+//respond method still has some issues with case and getting the second word and some edge scenarios but mostly workds
 
 import java.util.ArrayList;
 import java.util.Scanner; 
 import java.util.Random;
 
+//Class that implements Chatbot interface to have a conversation with the user
 class Conversation implements Chatbot {
 
   String input; //string to store user input
+  String randRes = ""; //string to store random response
+  String mirrorRes = ""; //string to store mirrored response
+  String response = ""; //string to store response
   ArrayList<String> transcript = new ArrayList<String>(); //array list to store conversation transcript 
   ArrayList<String> randRespond = new ArrayList<String>(); //array list to store canned responses
   
@@ -30,53 +29,98 @@ class Conversation implements Chatbot {
     randRespond.add("Mm-hmm.");
   }
 
-  //Starts and runs the conversation with the user
+  //Runs the conversation with the user
   public void chat() {
     Scanner in = new Scanner(System.in); // Create a Scanner object
-    Random rand = new Random(); //create random object
     
     System.out.println("Hello! How many rounds of conversation would you like to have?");
+
+    if(!in.hasNextInt()){ //check if input is an integer
+      System.out.println("Please enter a valid number of rounds.");
+      return;
+    }
     int rounds = in.nextInt(); //define rounds as user input
     in.nextLine(); //throw out the newline character
     
     System.out.println("Great! What would you like to talk about?");
 
-    //for loop that runs the number of rounds of conversation
+    //for loop that runs the specified # of rounds of convo
     for (int i = 0; i < rounds; i++) {
       input = in.nextLine(); //take in user input as string
-      //in.nextLine();
       transcript.add(input); //add user input to transcript
-      String randRes = randRespond.get(rand.nextInt(randRespond.size())); // Get a random response
-      System.out.println(randRes);
-      transcript.add(randRes); // Add to transcript
+      response = respond(input); //get response from respond method
+      transcript.add(response); // Add to transcript
+      System.out.println(response); //do i need to print the response here?
     }
-    System.out.println("Well, thanks for chatting!\n\nHere's a transcript of our conversation:");
+
+    System.out.println("Well, thanks for chatting!\n\nHere's a transcript of our conversation:\nWhat would you like to talk about?");
+    
+    printTranscript(); //call print transcript of conversation
+
     in.close(); //close scanner
   }
 
   //Prints transcript of conversation
   public void printTranscript() {
+     
     for (int i = 0; i < transcript.size(); i++) {
       System.out.println(transcript.get(i));
     }
-    System.out.println("Well, thanks for chatting!");
+      
+    System.out.println("Well, thanks for chatting!\nGoodbye!"); //includes goodbye message in transcript print and says bye
   }
 
-  /**
-   * Gives appropriate response (mirrored or canned) to user input
-   * @param inputString the users last line of input
-   * @return mirrored or canned response to user input  
-   */
+  
+  //Gives appropriate response (mirrored or canned) to user input
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    Random rand = new Random(); //create Random object
+    String tempRes = ""; //string to store response
+    boolean foundMirror = false; //boolean to check if mirror words found
+    String[][] mirrorWords = {{"i ", "you "}, {"you","I"},
+                              {"me", "you"}, //{"you", "me"},
+                              {"myself ", "yourself "}, {"yourself ", "myself "},
+                              {"my ", "your "}, {"your ", "my "},
+                              {"mine ", "yours "}, {"yours ", "mine "}, 
+                              {"i'd ","you'd "},//{"you'd ","I'd "},
+                              {"i've ","you've "},//{"you've ","I've "},
+                              {"i'll ","you'll "},//{"you'll ","I'll "},
+                              {"i'm ","you're "},//{"you're ","I'm "},
+                              {"am ","are "},{"are ","am "},
+    };
+    
+    //Make entire input lowercase so all mirror words trigger
+    inputString = inputString.toLowerCase();
+
+    //check input for mirror words and punctuation, replace as appropriate
+    for (int i = 0; i < mirrorWords.length; i++) {
+      if (inputString.contains(mirrorWords[i][0])) {
+        inputString = inputString.replace(mirrorWords[i][0], mirrorWords[i][1]); //replace mirror word with paired word
+        foundMirror = true; //mark that a mirror word was found
+      } 
+    }
+    //check if input ends with punctuation, if so replace with "?", if not just add "?"
+    if (inputString.endsWith("!")||inputString.endsWith("?")||inputString.endsWith(".")){
+      inputString = (inputString.substring(0, inputString.length()-1)+"?"); 
+    }else{
+      inputString += "?"; 
+    }
+    
+    tempRes = inputString; //set response to mirrored input
+
+    //check if response is empty (aka no mirror words found), if so rand response
+    if (!foundMirror) {
+      tempRes = randRespond.get(rand.nextInt(randRespond.size())); // Get a random response
+    }
+    
+    //capitalize first letter and return
+    tempRes = tempRes.substring(0,1).toUpperCase() + tempRes.substring(1);  
+    return tempRes;
   }
 
   public static void main(String[] arguments) {
 
     Conversation myConversation = new Conversation();
     myConversation.chat();
-    myConversation.printTranscript();
 
   }
 }
--- reflection.md
+++ reflection.md
@@ -7,11 +7,14 @@ Use this file to record your reflection on this assignment.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
 
 
+I worked with Aimen on this assignment in the beginning, we worked out a lot of syntax and technical stuff together to get started. (Including getting Java properly installed and setting up the basics like the constructor.)
 
-ChatGPT conversation(s):
+We attended the homework session that replaced class on Thursday morning which was helpful. I learned about the potential scanner bug (of not throwing out the newline) before I had a chance to run into it which was nice. 
 
-fixing new line issue
-https://chatgpt.com/share/67ae3d55-75f0-8012-a4d9-39babdb7851f 
+I like working in Java! I learned it my senior year of high school in AP Comp Sci A so it feels very familiar, although I have forgotten a lot of the details since its been two years. Regardless I am finding it much easier than Python because I never learned Python at all and even though its supposed to be relatively easy, I was having a hard time with it for A2. 
 
-randomizing responses to fix loop issue
-https://chatgpt.com/share/67ae3d55-75f0-8012-a4d9-39babdb7851f 
+Based on the teeny bit of Python I've been exposed to, it definitely has less particular syntax like not requring curly braces and semicolons, and not requiring you to declare variable types. In general though it seems like object oriented programming is pretty widely useful so it's nice that Java is set up for that. But I guess I think that because I haven't programmed much that would have been better in Python because of the classes I've taken...
+
+Once I got Java installed and had the basic parts of the program set up (and rembered how to write in Java) this went very smoothly. Getting the number of rounds and canned responses working wasn't bad at all, and figuring out how to optimize the respond method was fun. I used ChatGPT to help me fix some issues in the repond method- asking it for hints but not full code is very helpful. Here's the conversation: https://chatgpt.com/share/67af9c62-82e8-8012-a7ad-8c092ae08a1d .  
+
+It's hard for me to make a recommendation for someone else because they'd be completely learning Java on this assignment which I wasn't (for the most part), but I would tell people to do the warmup exercises to get started! I also found it helpful to have a list of Java's String methods up to figure out what I could use for the respond method.
